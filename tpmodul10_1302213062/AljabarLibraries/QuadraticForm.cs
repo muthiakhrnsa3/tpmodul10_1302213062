592a0d1300f1d268ae76b4a45fab3cb89962a7b0
Muthia Khairunnisa <127019755+muthiakhrnsa3@users.noreply.github.com>
2023-04-30 23:53:09 +0700

    Add files via upload

---
+++ tpmodul10_1302213062/AljabarLibraries/QuadraticForm.cs
@@ -0,0 +1,28 @@
+﻿namespace AljabarLibraries
+{
+    public class QuadraticForm
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double[] hasil = new double[2];
+
+            hasil[0] = (-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+            hasil[1] = (-b - Math.Sqrt((b * b) - (4 * a * c))) / (2 * a);
+            return hasil;
+        }
+        public static double[] HasilKuadrat(double[] kuadrat)
+        {
+            double a = kuadrat[0];
+            double b = kuadrat[1];
+            double[] hasil = new double[3];
+
+            hasil[0] = Math.Pow(a, 2);
+            hasil[1] = 2 * a * b;
+            hasil[2] = Math.Pow(b, 2);
+            return hasil;
+        }
+    }
+}
---
+++ tpmodul10_1302213062/TestAljabarLibraries/Program.cs
@@ -0,0 +1,10 @@
+﻿using AljabarLibraries;
+
+double[] APK = { 1, -3, -10 };
+double[] HS = { 2, -3 };
+
+double[] AkarPersamaanKuadrat = QuadraticForm.AkarPersamaanKuadrat(APK);
+double[] HasilKuadrat = QuadraticForm.HasilKuadrat(HS);
+
+Console.WriteLine("{" + AkarPersamaanKuadrat[0] + ", " + AkarPersamaanKuadrat[1] + "}");
+Console.WriteLine("{" + HasilKuadrat[0] + ", " + HasilKuadrat[1] + ", " + HasilKuadrat[2] + "}");
